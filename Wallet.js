6b45e94ea4f9511bd00ec70488303a5badfd2f7c
Krzysztof <107839551+Korbinek@users.noreply.github.com>
2025-05-04 14:33:09 +0200

    Update Wallet.js

--- Wallet.js
+++ Wallet.js
@@ -1,10 +1,10 @@
 class Wallet {
  constructor(money) {
   let _money = money;
-  //pobieranie aktualnej zawartości portfela
+  // Pobieranie aktualnej zawartości portfela
   this.getWalletValue = () => _money;
 
-  //Sprawdzanie czy użytkonik ma odpowiednią ilość środków
+  // Sprawdzanie czy użytkonik ma odpowiednią ilość środków
 
   this.checkCanPlay = value => {
    if (_money >= value) return true;
